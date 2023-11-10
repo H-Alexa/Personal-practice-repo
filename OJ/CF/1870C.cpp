40f26a72058a7314613bce81a0ec0c59cb074dbf
H-Alexa <tatineesarkersunom@gmail.com>
2023-11-10 07:36:04 +0600

    added ac solution of 1870C

--- OJ/CF/1870C.cpp
+++ OJ/CF/1870C.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define ino freopen("in.txt", "r", stdin);freopen("out.txt", "w", stdout);
 
 #define fr(i,n) for(i=0;i<n;i++)
-#define frs(i,a,n) for(i=a;i<n;i++)
+#define frs(i,a,n) for(i=a;i<=n;i++)
 #define frv(i,n) for(i=n-1;i>=0;i--)
 
 #define ne cout<<"\n";
@@ -24,11 +24,62 @@ using namespace std;
 #define isnttriangle(a,b,c) (a+b<c||a+c<b||b+c<a)
 #define pi acos(-1)
 #define siz 100009
-//int ar[siz];
+int ar[siz];
+int l[siz], r[siz];
+int fl[siz];
+int n;
+int pref(int pt,int val)
+{
+    for(int i=pt+1;i<=n;i++)
+    {
+        if(ar[i]<val) pt++;
+        else break;
+    }
+    return pt;
+}
+int suf(int pt, int val)
+{
+    for(int i=pt-1;i>0;i--)
+    {
+        if(ar[i]<val) pt--;
+        else break;
+    }
+    return pt;
+}
+void sol()
+{
+    memset(fl,0,sizeof(fl));
+    int k;
+    cin>>n>>k;
+    int i,j;
+    ar[0]=0;
+    ar[n+1]=0;
+    frs(i,1,n) {
+        cin>>ar[i];
+        if(fl[ar[i]]==0) fl[ar[i]]=i;
+    }
+    int ptl=0,ptr=n+1;
 
+    frs(i,1,k)
+    {
+        if(fl[i]==0) cout<<"0 ";
+        else{
+            ptl=pref(ptl,i);
+            ptr=suf(ptr,i);
+            cout<<2*(ptr-ptl-1)<<" ";
+        }
+    }
+    cout<<"\n";
+    return;
+}
 int main()
 {
-
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
     return 0;
 }
 
